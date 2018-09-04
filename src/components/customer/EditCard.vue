9d1e563932f8ff5c710b1973281c03bccc86b0e7
atroxic <atroxic@gmail.com>
2018-09-04 14:43:33 +0000

    Added loading bar to EditCard

--- src/components/customer/EditCard.vue
+++ src/components/customer/EditCard.vue
@@ -1,43 +1,46 @@
 <template>
   <v-form ref="cardForm" lazy-validation>
-    <v-layout row>
-      <v-flex>
-        <v-card>
-          <v-card-title><h3>Card Information</h3></v-card-title>
-          <v-card-text>
-            <v-layout row>
-              <v-flex xs8 md6>
-                <v-text-field
-                  v-model="card.cardNumber"
-                  label="Card Number"
-                  mask="credit-card"
-                  :type="hideCardNumber ? 'password' : 'text'"
-                  :append-icon="hideCardNumber ? 'fa-eye' : 'fa-eye-slash'"
-                  @click:append="hideCardNumber = !hideCardNumber"
-                  :readonly="viewonly"
-                ></v-text-field>
-              </v-flex>
-              <v-flex xs3 md2>
-                <v-text-field
-                  v-model="card.expiration"
-                  label="Expiration"
-                  mask="##/##"
-                  :readonly="viewonly"
-                ></v-text-field>
-              </v-flex>
-              <v-flex xs3 md2>
-                <v-text-field
-                  v-model="card.ccv"
-                  label="CCV"
-                  mask="####"
-                  :readonly="viewonly"
-                ></v-text-field>
-              </v-flex>
-            </v-layout>
-          </v-card-text>
-        </v-card>
-      </v-flex>
-    </v-layout>
+    <v-progress-linear v-if="loading" class="ma-0" :indeterminate="true"></v-progress-linear>
+    <v-container v-if="!loading" class="ma-0 pa-0">
+      <v-layout row>
+        <v-flex>
+          <v-card>
+            <v-card-title><h3>Card Information</h3></v-card-title>
+            <v-card-text>
+              <v-layout row>
+                <v-flex xs8 md6>
+                  <v-text-field
+                    v-model="card.cardNumber"
+                    label="Card Number"
+                    mask="credit-card"
+                    :type="hideCardNumber ? 'password' : 'text'"
+                    :append-icon="hideCardNumber ? 'fa-eye' : 'fa-eye-slash'"
+                    @click:append="hideCardNumber = !hideCardNumber"
+                    :readonly="viewonly"
+                  ></v-text-field>
+                </v-flex>
+                <v-flex xs3 md2>
+                  <v-text-field
+                    v-model="card.expiration"
+                    label="Expiration"
+                    mask="##/##"
+                    :readonly="viewonly"
+                  ></v-text-field>
+                </v-flex>
+                <v-flex xs3 md2>
+                  <v-text-field
+                    v-model="card.ccv"
+                    label="CCV"
+                    mask="####"
+                    :readonly="viewonly"
+                  ></v-text-field>
+                </v-flex>
+              </v-layout>
+            </v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </v-form>
 </template>
 
